b1a0259ed8db693c0686a08592f7e1588ff7e146
Sumit Attri <sumitattri165@gmail.com>
2025-01-05 19:57:58 +0530

    added datatypes file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use stric"; // treat all js code as newer version of js
+
+//alert(3+3); //we are  using node.js and not the browser
+
+let name = "Sumit"
+let age = 22
+let isMale = true
+let state;
+
+/* following are some datatypes
+number datatype => range is 2 to power 53
+bigint datatype 
+string datatype => can be single or double quotes '' or ""
+boolean datatype => true or false
+null datatype =>  standalone value
+undefined datatype=>  that has been declared but not assigned a value
+ symbol datatype => unique and immutable value
+
+object datatype 
+*/
+
+console.log(typeof"Sumit");
+console.log(typeof age);
+console.log(null);      //object
+console.log (undefined); //undefined

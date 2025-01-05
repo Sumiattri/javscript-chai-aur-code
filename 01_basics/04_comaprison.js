14f8ec3b6bc61a8598bcb6d81854cde6176e8689
Sumit Attri <sumitattri165@gmail.com>
2025-01-05 21:08:07 +0530

    camparison basics

---
+++ 01_basics/04_comaprison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);  //avoid all these type of meaningless comparison
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === (strict check, also check datatype and gives strict answer)
+
+console.log("2" === 2);
